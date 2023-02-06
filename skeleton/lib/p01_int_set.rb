fa90ba5d8c5de3b0dd03e44027005a09e097b22f
jahinbhuiyan <jahinbarobhuiyan@gmail.com>
2023-02-06 14:52:10 -0500

    Skeleton

--- skeleton/lib/p01_int_set.rb
+++ skeleton/lib/p01_int_set.rb
@@ -1,14 +1,35 @@
 class MaxIntSet
+  attr_accessor :store
   def initialize(max)
+    @store = Array.new(max, false) 
   end
 
   def insert(num)
+      if num >= @store.length
+        raise ArgumentError.new("Out of bounds")
+      end
+      if num < 0
+        raise ArgumentError.new("Out of bounds")
+      end
+      @store[num] = true
   end
 
   def remove(num)
+    if num > @store.length
+      raise "OOB"
+  end
+  if num < 0
+      raise "OOB"
+  end
+  @store[num] = false
   end
 
   def include?(num)
+      if @store[num] == false
+        return false
+      else
+        return true
+      end
   end
 
   private
@@ -22,17 +43,25 @@ end
 
 
 class IntSet
+  attr_reader :store
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
   end
 
   def insert(num)
+    bucket_idx = num % 20
+        @store[bucket_idx] << num
   end
 
   def remove(num)
+    bucket_idx = num % 20
+    removing_ele_idx = @store[bucket_idx].index(num)
+    @store[bucket_idx] = @store[bucket_idx][0...removing_ele_idx] + @store[bucket_idx][removing_ele_idx + 1..-1] 
   end
 
   def include?(num)
+    bucket_idx = num % 20
+    @store[bucket_idx].include?(num)
   end
 
   private
